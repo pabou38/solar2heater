955dca317eab9d2f246df30a09269c436bfd44f6
pascal boudalier <pboudalier@gmail.com>
2023-02-27 09:11:53 +0100

    Add files via upload
    add secret

---
+++ secret - template.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+BLYNK_TEMPLATE_ID = ""
+BLYNK_DEVICE_NAME = ""
+BLYNK_AUTH_TOKEN  = "
+
+# myenergy 
+hub_serial = ""
+hub_pwd = ""
+director_url = ""
+
+# envoy-s host IP
+envoy_host = '192.168.1.182'
+
+# /home is web gui
+# /stream/meter   non stoppable streaming
+# /production.json single shot
+
+# envoy installer password. for streaming API. not needed for production.json
+envoy_password = ''
+envoy_user = ''
+
+pushover = {
+"pushover_token" : "",
+"pushover_user" : ""
+}
+
+# gmail
+email_passwd = ""
+dest_email = ""
+sender_email = ""
